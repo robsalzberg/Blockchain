fda40682be2113c2e1cc99f13d4afd36f4b61002
robsalzberg <robsalzberg@gmail.com>
2019-08-07 22:51:13 -0400

    Posted new proof to mine endpoint and returned a message along with adding to mine count

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -49,8 +49,22 @@ if __name__ == '__main__':
     # Run forever until interrupted
     while True:
         # TODO: Get the last proof from the server and look for a new one
+        r = requests.get(url=node + "/last_proof")
         # TODO: When found, POST it to the server {"proof": new_proof}
+        # the data is extracted in JSON format
+        data = r.json()
+
+        last_proof = data["last_proof"]
+
+        new_proof = proof_of_work(last_proof)
+        # TODO: We're going to have to research how to do a POST in Python
+        # HINT: Research `requests` and remember we're sending our data as JSON
+        proof_request = {"proof": new_proof}
+        r = requests.post(url=node + "/mine", json=proof_request)
+        print(r.json()["message"])
         # TODO: If the server responds with 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        if r.json()["message"] == "New Block Forged":
+            mined += 1
+            print("The total number of Lambda Coins mined is: ", mined)
