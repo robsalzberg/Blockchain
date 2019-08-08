8535e9eadf615b4c94c059ccd5953339299a6bc2
robsalzberg <robsalzberg@gmail.com>
2019-08-07 22:03:49 -0400

    Modify the mine endpoint to instead receive and validate or reject a new proof sent by a client.

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -5,7 +5,37 @@ import sys
 
 
 # TODO: Implement functionality to search for a proof 
+def proof_of_work(self, last_proof):
+    """
+    Proof of Work Algorithm
+    Find a number p such that hash(last_block_string, p) contains 6 leading
+    zeroes
+    """
 
+    proof = 0
+
+    while self.valid_proof(last_proof, proof) is False:
+        proof += 1
+
+    return proof
+
+
+def valid_proof(last_proof, proof):
+    """
+    Does hash(block_string, proof) contain 6 leading zeroes?
+    """
+    # build string to hash
+    guess = f"{last_proof}{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    # use hash function
+    beginning = guess_hash[:6]
+
+    # check if 6 leading 0's in hash result
+    if beginning == "000000":
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     # What node are we interacting with?
